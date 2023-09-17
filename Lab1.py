c660304a3ed02101c5d85ffbffeff1531bf221a9
Vladyslav Dushko <vladyslav.dusko2gmail.com>
2023-09-17 16:45:16 +0300

    first modification

---
+++ Lab1.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+n = int(input("Введіть n: "))
+
+if n < 0:
+    print("Введіть додатне число.")
+else:
+    result = fibonacci(n)
+    print(f"{n}-е число Фібоначчі: {result}")
